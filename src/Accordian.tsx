f67473181ad6c934726a55c38dbd395f7f892af2
fon <anatsaya.lee@gmail.com>
2024-05-09 14:42:17 +0700

    Create single toggle

--- src/Accordian.tsx
+++ src/Accordian.tsx
@@ -1,30 +1,46 @@
 import "./Accordian.css";
 
-type Data = {
+export type Data = {
   id: string;
   question: string;
   answer: string;
+  isToggled: boolean;
 };
 
 type AccordianProp = {
   data: Data[];
+  setData: React.Dispatch<React.SetStateAction<Data[]>>;
 };
 
-const Accordian: React.FC<AccordianProp> = ({ data }) => {
+const Accordian: React.FC<AccordianProp> = ({ data, setData }) => {
+  const handleToggle = (card: Data) => {
+    setData((prev: Data[]) => {
+      return prev.map((item: Data) => {
+        return item.id === card.id
+          ? { ...item, isToggled: !item.isToggled }
+          : { ...item, isToggled: false };
+      });
+    });
+  };
+
   return (
     <div className="Accordian ">
       <div className="Accordian__box">
         <h2>Accordian</h2>
       </div>
       <div className="Accordian__cards">
-        {data.map((each) => {
+        {data.map((eachCard) => {
           return (
-            <div className="Accordian__box" key={each.id}>
+            <div className="Accordian__box" key={eachCard.id}>
               <div className="Accordian__cards__question">
-                <h3>{each.question}</h3>
-                <button>+</button>
+                <h3>{eachCard.question}</h3>
+                <button onClick={() => handleToggle(eachCard)}>
+                  {eachCard.isToggled ? "-" : "+"}
+                </button>
               </div>
-              <p>{each.answer}</p>
+              <p style={{ display: eachCard.isToggled ? "block" : "none" }}>
+                {eachCard.answer}
+              </p>
             </div>
           );
         })}
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Accordian from "./Accordian";
 import { data } from "./data";
+import { Data } from "./Accordian";
 
 function App() {
+  const [dataAccordians, setDataAccordians] = useState<Data[]>(data);
   return (
     <div className="App">
-      <Accordian data={data} />
+      <Accordian data={dataAccordians} setData={setDataAccordians} />
     </div>
   );
 }
